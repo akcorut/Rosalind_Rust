4980fab12b914745d5550265ff862ebf45a29ee8
Kivanc Corut <kivanc.corut@uga.edu>
2020-12-14 17:50:16 -0500

    Change var name

--- count_dna_nuc/src/main.rs
+++ count_dna_nuc/src/main.rs
@@ -2,32 +2,29 @@ use std::env;
 use std::fs;
 
 fn main() {
-        // --snip--
-        let args: Vec<String> = env::args().collect();
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
 
-        let filename = &args[1];
+    let input_dna = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("{}", input_dna);
+
+    let mut A_count = 0;
+    let mut T_count = 0;
+    let mut G_count = 0;
+    let mut C_count = 0;
+    let mut _X_count = 0;
     
-
-        let contents = fs::read_to_string(filename)
-            .expect("Something went wrong reading the file");
-
-        println!("{}", contents);
-
-        let mut A_count = 0;
-        let mut T_count = 0;
-        let mut G_count = 0;
-        let mut C_count = 0;
-        let mut _X_count = 0;
-
-        for nuc in contents.chars() {
-            match nuc {
-                'A' => A_count +=1,
-                'T' => T_count +=1,
-                'G' => G_count +=1,
-                'C' => C_count +=1,
-                _ => _X_count +=1,
-            }
+    for nuc in input_dna.chars() {
+        match nuc {
+            'A' => A_count +=1,
+            'T' => T_count +=1,
+            'G' => G_count +=1,
+            'C' => C_count +=1,
+            _ => _X_count +=1,
         }
-
-        println!("A: {}, T: {}, G: {}, C: {}", A_count, T_count, G_count, C_count);
+    }
+    println!("A: {}, T: {}, G: {}, C: {}", A_count, T_count, G_count, C_count);
 }
